f535cec2cdd8e4ee8a72d6e6ee15d6359aefe061
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-01-09 19:07:31 +0100

    add model cats

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+
+class Cat(models.Model):
+    """Class representing a cat"""
+
+    name = models.CharField(max_length=30)
+    date_of_birth = models.CharField(max_length=30) # Figure out how to use actual dates
+    initial_weight = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+        help_text="Enter a positive number"
+    )
+    def __str__(self):
+        return f"{self.name}"
