bd0f624b7cafa06d5cb5c5daffae658f51497965
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-01-10 16:12:28 +0100

    add weight entry

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Cat, WeightEntry
+
 # Register your models here.
+
+admin.site.register(Cat)
+admin.site.register(WeightEntry)
--- tracker/models.py
+++ tracker/models.py
@@ -1,15 +1,45 @@
+"""Django models, declares data"""
+
 from django.db import models
 from django.core.validators import MinValueValidator
+from .validators import validate_not_in_future
 # Create your models here.
 
 class Cat(models.Model):
     """Class representing a cat"""
-
     name = models.CharField(max_length=30)
-    date_of_birth = models.CharField(max_length=30) # Figure out how to use actual dates
+    date_of_birth = models.DateField(
+        validators=[validate_not_in_future],
+        help_text="Enter the measurement's date"
+        )
+    
     initial_weight = models.FloatField(
         validators=[MinValueValidator(0.0)],
         help_text="Enter a positive number"
     )
     def __str__(self):
         return f"{self.name}"
+    
+class WeightEntry(models.Model):
+    """Class representing a weight entry"""
+
+    class Meta:
+        """Meta class"""
+        verbose_name_plural="weight entries"
+    cat = models.ForeignKey(
+        Cat,
+        on_delete=models.CASCADE,  # Deletes weight entries if the associated cat is deleted
+        related_name="weight_entries"  # Allows accessing a cat's weight entries as `cat.weight_entries`
+    )
+
+    date = models.DateField(
+        validators=[validate_not_in_future],
+        help_text="Enter the measurement's date"
+    )
+    weight = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+        help_text="Enter a positive number"
+    )
+
+    def __str__(self):
+        return f"Weight: {self.weight} on {self.date} for {self.cat.name}"
